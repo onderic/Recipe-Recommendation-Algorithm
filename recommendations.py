a3b38a3662c060b03274e7751753f5be1d9380d0
onderic <ondericollins80@gmail.com>
2023-10-18 13:46:40 +0300

    recommendation fix

--- recommendations.py
+++ recommendations.py
@@ -5,8 +5,8 @@ from sklearn.metrics.pairwise import linear_kernel
 # Function to recommend recipes based on cosine similarity
 def recommend_recipes(recipe_id, recipes):
     data = [[recipe['recipe_id'], recipe['Ingredients']] for recipe in recipes]
-
-    data = pd.DataFrame(data, columns=['recipe_id', 'Ingredients'])
+    
+    data = pd.DataFrame(data,  columns=['recipe_id', 'Ingredients'])
     data['Ingredients'] = data['Ingredients'].fillna('')
     data["Ingredients"] = data["Ingredients"].str.lower()
     data["recipe_text"] = data["recipe_id"].astype(str) + " " + data["Ingredients"]
@@ -14,36 +14,24 @@ def recommend_recipes(recipe_id, recipes):
     # Remove any rows with NaN values in the "recipe_text" column
     data = data.dropna(subset=['recipe_text'])
 
-    print("Data with NaN values removed:")
-    print(data)
-
     tfidf_vectorizer = TfidfVectorizer(stop_words="english")
     tfidf_matrix = tfidf_vectorizer.fit_transform(data["recipe_text"])
 
-    print("TF-IDF matrix:")
-    print(tfidf_matrix)
-
     # Calculate the cosine similarity matrix
-    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
-
-    print("Cosine Similarity Matrix:")
-    print(cosine_sim)
+    cosine_sim = linear_kernel(tfidf_matrix,tfidf_matrix)
 
     # Get recipe IDs
-    recipe_ids = data["recipe_id"].tolist()
+    recipe_ids = data['recipe_id'].tolist()
 
     if recipe_id not in recipe_ids:
         print(f"Recipe ID '{recipe_id}' does not exist.")
         return []
-
+    
     recipe_idx = recipe_ids.index(recipe_id)
-
+    
     # Get the pairwise similarity scores
     similar_recipes = list(enumerate(cosine_sim[recipe_idx]))
-
-    print("Similarity Scores:")
-    print(similar_recipes)
-
+    
     # Sort recipes by similarity score
     similar_recipes = sorted(similar_recipes, key=lambda x: x[1], reverse=True)
 
@@ -52,6 +40,7 @@ def recommend_recipes(recipe_id, recipes):
 
     # Retrieve the IDs of recommended recipes
     recommended_ids = [recipe_ids[i[0]] for i in top_recipes]
-    print("recommended_ids:", recommended_ids)
 
     return recommended_ids
+
+    
